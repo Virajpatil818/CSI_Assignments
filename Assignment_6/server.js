9a75178ac445604df9a7a1607d292fa309283788
Viraj Balasaheb Patil <95068416+Virajpatil818@users.noreply.github.com>
2024-06-29 23:10:21 +0530

    Add files via upload

---
+++ Assignment_6/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+const port = 3000;
+
+// Middleware
+app.use(bodyParser.json());
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/restapi', { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Routes
+const userRoutes = require('./routes/users');
+app.use('/users', userRoutes);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
